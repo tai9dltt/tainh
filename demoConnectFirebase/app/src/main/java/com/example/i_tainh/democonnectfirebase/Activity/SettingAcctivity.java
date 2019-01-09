83a1cbe3ea9d7a719ef6fb87df9f8d7c070832a6
tainh <tai9dll@gmail.com>
2019-01-09 17:26:23 +0700

    update last day

--- demoConnectFirebase/app/src/main/java/com/example/i_tainh/democonnectfirebase/Activity/SettingAcctivity.java
+++ demoConnectFirebase/app/src/main/java/com/example/i_tainh/democonnectfirebase/Activity/SettingAcctivity.java
@@ -1,21 +1,33 @@
 package com.example.i_tainh.democonnectfirebase.Activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.i_tainh.democonnectfirebase.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -25,10 +37,13 @@ public class SettingAcctivity extends AppCompatActivity {
     private TextView settingDisplayName;
     private TextView settingDisplayStatus;
     private Button settingChangeProfileImangeButton;
-    private Button settingChangeStausButton;
+    private Button settingChangeStatusButton;
     private final static int GALLAY_PICK = 1;
     private DatabaseReference getUserReference;
     private FirebaseAuth mAuth;
+    private StorageReference storeProfileImageStoreageRef;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +53,14 @@ public class SettingAcctivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         String online_user_id = mAuth.getCurrentUser().getUid();
         getUserReference = FirebaseDatabase.getInstance().getReference().child("Users").child(online_user_id);
+        storeProfileImageStoreageRef = FirebaseStorage.getInstance().getReference().child("Profile_Images");
 
-        settingDisplayImage = findViewById(R.id.img_user);
+
+        settingDisplayImage = (CircleImageView) findViewById(R.id.img_user);
         settingDisplayName = findViewById(R.id.textViewUsername);
         settingDisplayStatus = findViewById(R.id.user_status);
         settingChangeProfileImangeButton = findViewById(R.id.setting_chan_imangeProfile_button);
-        settingChangeStausButton = findViewById(R.id.setting_change_profile_status_button);
+        settingChangeStatusButton = findViewById(R.id.setting_change_profile_status_button);
 
         getUserReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -51,13 +68,13 @@ public class SettingAcctivity extends AppCompatActivity {
                 String name = dataSnapshot.child("user_name").getValue().toString();
                 String status = dataSnapshot.child("user_status").getValue().toString();
                 String image = dataSnapshot.child("user_image").getValue().toString();
-                String thumb = dataSnapshot.child("user_thumb_image").getValue().toString();
+                String thumb_image = dataSnapshot.child("user_thumb_image").getValue().toString();
 
                 settingDisplayName.setText(name);
                 settingDisplayStatus.setText(status);
+                Picasso.get().load(image).memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE).into(settingDisplayImage);
 
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
@@ -78,6 +95,45 @@ public class SettingAcctivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLAY_PICK && resultCode == RESULT_OK && data != null){
+            Uri imageUri = data.getData();
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1,1)
+                    .start(this);
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                 Uri resultUri = result.getUri();
+
+                String user_id = mAuth.getCurrentUser().getUid();
+                StorageReference  filePath = storeProfileImageStoreageRef.child(user_id + ".jpg");
+
+                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SettingAcctivity.this,"Saving your profile image to firebase",Toast.LENGTH_SHORT).show();
+
+                            String downloadUrl = task.getResult().getStorage().getDownloadUrl().toString();
+                            Log.d("downloadUrl", downloadUrl);
+                            getUserReference.child("user_image").setValue(downloadUrl).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            Toast.makeText(SettingAcctivity.this,"Image upload successful",Toast.LENGTH_SHORT).show();
+
+                                        }
+                                    });
+                        }else {
+                            Toast.makeText(SettingAcctivity.this,"Something wrong, try again",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
 
     }
 }
